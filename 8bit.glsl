532f4bdabfb5c0e82963adc5388aca2f4ed6c1e9
DenisBelmondo <steakwilsomusic@gmail.com>
2024-04-27 14:39:22 -0600

    lch crap

--- 8bit.glsl
+++ 8bit.glsl
@@ -1,19 +1,19 @@
 // Copyright 2022 Rachael Alexanderson
-// 
+//
 // Redistribution and use in source and binary forms, with or without
 // modification, are permitted provided that the following conditions are met:
-// 
+//
 // 1. Redistributions of source code must retain the above copyright notice,
 //    this list of conditions and the following disclaimer.
-// 
+//
 // 2. Redistributions in binary form must reproduce the above copyright notice,
 //    this list of conditions and the following disclaimer in the documentation
 //    and/or other materials provided with the distribution.
-// 
+//
 // 3. Neither the name of the copyright holder nor the names of its
 //    contributors may be used to endorse or promote products derived from this
 //    software without specific prior written permission.
-// 
+//
 // THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
 // AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
 // IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
@@ -26,6 +26,22 @@
 // ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 // POSSIBILITY OF SUCH DAMAGE.
 
+
+#define PI 3.14159365
+#define TAU 6.28318531
+
+//cheaply lerp around a circle
+float lerpAng(in float a, in float b, in float x)
+{
+    float ang = mod(mod((a-b), TAU) + PI*3., TAU)-PI;
+    return ang*x+b;
+}
+
+vec3 lerpLch(in vec3 a, in vec3 b, in float x)
+{
+    float hue = lerpAng(a.z, b.z, x);
+    return vec3(mix(b.xy, a.xy, x), hue);
+}
 
 vec4 paldownmix(vec4 c)
 {
@@ -100,12 +116,123 @@ float brightness(vec3 c)
 	return pow(dot(pow(vec3(c), vec3(2.2)), vec3(0.2126, 0.7152, 0.0722)), 1.0/2.2);
 }
 
+#define diag3(v) mat3((v).x, 0.0, 0.0, 0.0, (v).y, 0.0, 0.0, 0.0, (v).z)
+#define xyY_to_XYZ(x, y, Y) vec3(Y/y*x, Y, Y/y*(1.0 - x - y))
+#define xy_to_XYZ(x, y) vec3(x/y, 1.0, (1.0 - x - y)/y)
+#define xy_to_xyz(x, y) vec3(x, y, 1.0 - x - y)
+
+const mat3 BFD = mat3(0.8951, -0.7502, 0.0389, 0.2664, 1.7135, -0.0685, -0.1614, 0.0367, 1.0296);
+
+const vec3 D50 = xy_to_XYZ(0.34567, 0.35850);
+const vec3 D65 = xy_to_XYZ(0.31271, 0.32902);
+const mat3 D65_TO_D50 = inverse(BFD)*diag3((BFD*D50)/(BFD*D65))*BFD;
+
+const mat3 sRGB = mat3(xy_to_XYZ(0.64, 0.33), xy_to_XYZ(0.30, 0.60), xy_to_XYZ(0.15, 0.06));
+const mat3 sRGB_TO_XYZ_D65 = sRGB*diag3(inverse(sRGB)*D65);
+const mat3 sRGB_TO_XYZ_D50 = D65_TO_D50*sRGB_TO_XYZ_D65;
+const mat3 XYZ_D65_TO_sRGB = inverse(sRGB_TO_XYZ_D65);
+const mat3 XYZ_D50_TO_sRGB = inverse(sRGB_TO_XYZ_D50);
+
+vec3 sRGB_OETF(vec3 c) {
+	vec3 a = 12.92*c;
+	vec3 b = 1.055*pow(c, vec3(1.0/2.4)) - 0.055;
+	return mix(a, b, greaterThan(c, vec3(0.00313066844250063)));
+}
+
+vec3 sRGB_EOTF(vec3 c) {
+	vec3 a = c/12.92;
+	vec3 b = pow((c + 0.055)/1.055, vec3(2.4));
+	return mix(a, b, greaterThan(c, vec3(0.0404482362771082)));
+}
+
+// LCh(ab) ↔ Lab ↔ XYZ ↔ sRGB
+
+vec3 XYZ_to_Lab(vec3 XYZ, vec3 XYZw) {
+	vec3 t = XYZ/XYZw;
+	vec3 a = pow(t, vec3(1.0/3.0));
+	vec3 b = 841.0/108.0*t + 4.0/29.0;
+	vec3 c = mix(b, a, greaterThan(t, vec3(216.0/24389.0)));
+	return vec3(1.16*c.y - 0.16, vec2(5.0, 2.0)*(c.xy - c.yz));
+}
+
+vec3 Lab_to_XYZ(vec3 Lab, vec3 XYZw) {
+	float L = (Lab.x + 0.16)/1.16;
+	vec3 t = vec3(L + Lab.y/5.0, L, L - Lab.z/2.0);
+	vec3 a = pow(t, vec3(3.0));
+	vec3 b = 108.0/841.0*(t - 4.0/29.0);
+	return XYZw*mix(b, a, greaterThan(t, vec3(6.0/29.0)));
+}
+
+vec3 LCh_to_Lab(vec3 LCh) {
+	return vec3(LCh.x, LCh.y*vec2(cos(LCh.z), sin(LCh.z)));
+}
+
+vec3 Lab_to_LCh(vec3 Lab) {
+	return vec3(Lab.x, length(Lab.yz), atan(Lab.z, Lab.y));
+}
+
+vec3 sRGB_to_Lab(vec3 sRGB) {
+	return XYZ_to_Lab(sRGB_TO_XYZ_D50*sRGB, D50);
+}
+
+vec3 Lab_to_sRGB(vec3 Lab) {
+	return XYZ_D50_TO_sRGB*Lab_to_XYZ(Lab, D50);
+}
+
+// LCh(uv) ↔ Luv ↔ XYZ ↔ sRGB
+
+#define XYZ_to_uv(XYZ) vec2(4.0, 9.0)*XYZ.xy/(XYZ.x + 15.0*XYZ.y + 3.0*XYZ.z)
+#define xy_to_uv(xy) vec2(4.0, 9.0)*xy/(-2.0*xy.x + 12.0*xy.y + 3.0)
+#define uv_to_xy(uv) vec2(9.0, 4.0)*uv/(6.0*uv.x - 16.0*uv.y + 12.0)
+
+vec3 XYZ_to_Luv(vec3 XYZ, vec3 XYZw) {
+	float Y = XYZ.y/XYZw.y;
+	float L = Y > 216.0/24389.0 ? 1.16*pow(Y, 1.0/3.0) - 0.16 : 24389.0/2700.0*Y;
+	return vec3(L, 13.0*L*(XYZ_to_uv(XYZ) - XYZ_to_uv(XYZw)));
+}
+
+vec3 Luv_to_XYZ(vec3 Luv, vec3 XYZw) {
+	vec2 uv = Luv.yz/(13.0*Luv.x) + XYZ_to_uv(XYZw);
+	float Y = Luv.x > 0.08 ? pow((Luv.x + 0.16)/1.16, 3.0) : 2700.0/24389.0*Luv.x;
+	float X = (9.0*uv.x)/(4.0*uv.y);
+	float Z = (12.0 - 3.0*uv.x - 20.0*uv.y)/(4.0*uv.y);
+	return XYZw.y*vec3(Y*X, Y, Y*Z);
+}
+
+vec3 LCh_to_Luv(vec3 LCh) {
+	return vec3(LCh.x, LCh.y*vec2(cos(LCh.z), sin(LCh.z)));
+}
+
+vec3 Luv_to_LCh(vec3 Luv) {
+	return vec3(Luv.x, length(Luv.yz), atan(Luv.z, Luv.y));
+}
+
+vec3 sRGB_to_Luv(vec3 sRGB) {
+	return XYZ_to_Luv(sRGB_TO_XYZ_D65*sRGB, D65);
+}
+
+vec3 Luv_to_sRGB(vec3 Luv) {
+	return XYZ_D65_TO_sRGB*Luv_to_XYZ(Luv, D65);
+}
+
 void main()
 {
 	vec4 c = clamp(texture(InputTexture, TexCoord), vec4(0.0, 0.0, 0.0, 0.0), vec4(1.0, 1.0, 1.0, 1.0));
+	// vec2 txc = TexCoord * textureSize(InputTexture, 0);
+	vec3 Lab2 = sRGB_to_Lab(c.rgb);
+	vec3 LCh2 = Lab_to_LCh(Lab2);
 
-	vec2 txc = TexCoord * textureSize(InputTexture, 0);
+	vec3 downmixedColor = Lab_to_LCh(sRGB_to_Lab(downmix(c).rgb));
 
+	vec3 poop05 = lerpLch(LCh2, downmixedColor, 0.5);
+
+	vec3 poop = LCh_to_Lab(poop05);
+	vec3 poop2 = Lab_to_sRGB(poop);
+
+	// FragColor = texture(InputTexture, TexCoord);
+	FragColor.rgb = poop2;
+
+	/*
 	switch (c_mode)
 	{
 	default:
@@ -158,6 +285,5 @@ void main()
 			float bri4 = max(brightness(vec3(o4)), 0.0001);
 			FragColor = o4 * bri1 / bri4;
 		}
-	}
+	} */
 }
-
